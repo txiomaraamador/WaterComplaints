ba77337994c8ae7180ee824648c311ab0ecfe9d1
VladAgui2001 <vlad.aguiluz5c@gmail.com>
2023-06-02 17:24:12 -0600

    Reparado error con variable "ReportTime" 	modified:   WaterLeakReportCollector.cs 	modified:   bin/Debug/net7.0/WaterComplaints.dll 	modified:   bin/Debug/net7.0/WaterComplaints.pdb 	modified:   obj/Debug/net7.0/WaterComplaints.dll 	modified:   obj/Debug/net7.0/WaterComplaints.pdb 	modified:   obj/Debug/net7.0/ref/WaterComplaints.dll 	modified:   obj/Debug/net7.0/refint/WaterComplaints.dll

--- WaterLeakReportCollector.cs
+++ WaterLeakReportCollector.cs
@@ -107,7 +107,7 @@ public class WaterLeakReport
     public string Description { get; set; }
     public string GeoreferencedLocation { get; set; }
     public int Severity { get; set; }
-    public TimeSpan ReportTime { get; set; }
+    public DateTime ReportTime { get; set; }
     public string PhoneNumber { get; set; }
     public string Email { get; set; }
     public int Age { get; set; }

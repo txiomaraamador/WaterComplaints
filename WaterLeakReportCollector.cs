875ff12a95bd833b393283783d5a46b65ae57684
estebansg1 <estebandesa0@gmail.com>
2023-06-14 18:44:25 -0600

    reorder classes

--- WaterLeakReportCollector.cs
+++ WaterLeakReportCollector.cs
@@ -114,18 +114,3 @@ public class WaterLeakReportCollector
         return WaterLeakReport;
     }
 }
-
-public class WaterLeakReport
-{
-    public string Location { get; set; }
-    public DateTime ReportDate { get; set; }
-    public string vanishingtime { get; set; }
-    public string ReporterName { get; set; }
-    public string Description { get; set; }
-    public string GeoreferencedLocation { get; set; }
-    public int Severity { get; set; }
-    public DateTime ReportTime { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public int Age { get; set; }
-}
